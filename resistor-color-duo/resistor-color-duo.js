6eb74423ba8d5a9da8d90ac0ea52162c55d723d9
Oluwaseun Oyebade <motdde@users.noreply.github.com>
2020-06-17 00:44:48 +0100

    Completed Resistor-color-duo

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,15 @@
+export const decodedValue = ([a, b]) =>
+  parseInt(`${COLORS.indexOf(a)}${COLORS.indexOf(b)}`);
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
